14c89bc85a7cc4f9ce34b7325ee34c1b0c1046fe
Admin <hirenbambhaniya120@gmail.com>
2024-11-25 13:38:12 +0530

    11/25

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:animation_daily_dask/screens/airoplane_screen.dart';
 import 'package:animation_daily_dask/screens/greeting_animation.dart';
 import 'package:animation_daily_dask/screens/hero_animation.dart';
 import 'package:animation_daily_dask/screens/puzzle.dart';
+import 'package:animation_daily_dask/screens/rotation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: PlanetRotation(),
     );
   }
 }
---
+++ lib/screens/rotation.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class PlanetRotation extends StatefulWidget {
+  const PlanetRotation({super.key});
+
+  @override
+  State<PlanetRotation> createState() => _PlanetRotationState();
+}
+
+class _PlanetRotationState extends State<PlanetRotation>
+    with TickerProviderStateMixin {
+  late AnimationController animationController;
+  int duration = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  void initialize() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: duration),
+      lowerBound: 0,
+      upperBound: 1,
+    )..repeat();
+  }
+
+  void updateDuration(int change) {
+    setState(() {
+      duration = (duration + change).clamp(1, 60);
+      initialize();
+      print(duration);
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+            animation: animationController,
+            builder: (context, child) {
+              return RotationTransition(
+                turns: animationController,
+                child: Center(
+                  child: Container(
+                    height: 280,
+                    width: 280,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/earth.png'))),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 29.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FloatingActionButton(
+              heroTag: 2,
+              onPressed: () {
+                updateDuration(1); // Decrease duration
+              },
+              child: const Icon(Icons.remove),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            FloatingActionButton(
+              heroTag: 1,
+              onPressed: () {
+                updateDuration(-1); // Increase duration
+              },
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
